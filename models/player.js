80fa7365d09b8ad4e9388198513ad65cdb094de4
Khangwal <anujkhangwal30@gmail.com>
2026-07-26 12:37:17 +0530

    Update player.js

--- models/player.js
+++ models/player.js
@@ -1,15 +1,14 @@
 import * as THREE from "https://cdn.jsdelivr.net/npm/three@0.160/build/three.module.js";
+import { createBody } from "./body.js";
 
 export function createPlayer() {
 
-    const player = new THREE.Mesh(
-        new THREE.BoxGeometry(2, 2, 4),
-        new THREE.MeshStandardMaterial({
-            color: 0xff0000
-        })
-    );
+    const car = new THREE.Group();
 
-    player.position.set(0, 1, 0);
+    const body = createBody();
+    car.add(body);
 
-    return player;
+    car.position.set(0, 0, 0);
+
+    return car;
 }
