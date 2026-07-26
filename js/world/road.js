06294938796554263c94d48728052026a2d91bb0
Khangwal <anujkhangwal30@gmail.com>
2026-07-26 20:58:30 +0530

    Update road.js

--- js/world/road.js
+++ js/world/road.js
@@ -12,6 +12,19 @@ export function createRoad(scene) {
     road.position.set(0, 0.1, 0);
 
     scene.add(road);
+// Center lane lines
+for (let i = -140; i < 150; i += 10) {
 
+    const line = new THREE.Mesh(
+        new THREE.BoxGeometry(0.4, 0.03, 5),
+        new THREE.MeshStandardMaterial({
+            color: 0xffffff
+        })
+    );
+
+    line.position.set(0, 0.22, i);
+
+    scene.add(line);
+}
     return road;
 }
