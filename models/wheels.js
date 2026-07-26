da5b59878bd9382d605d7278f6172b834a9ecb25
Khangwal <anujkhangwal30@gmail.com>
2026-07-26 12:51:20 +0530

    Update wheels.js

--- models/wheels.js
+++ models/wheels.js
@@ -1,1 +1,41 @@
+import * as THREE from "https://cdn.jsdelivr.net/npm/three@0.160/build/three.module.js";
 
+export function createWheels() {
+
+    const wheels = new THREE.Group();
+
+    function wheel(x, z) {
+
+        const tyre = new THREE.Mesh(
+
+            new THREE.CylinderGeometry(
+                0.5,
+                0.5,
+                0.4,
+                32
+            ),
+
+            new THREE.MeshStandardMaterial({
+                color: 0x111111,
+                roughness: 1
+            })
+
+        );
+
+        tyre.rotation.z = Math.PI / 2;
+
+        tyre.position.set(x, 0.5, z);
+
+        wheels.add(tyre);
+
+    }
+
+    wheel(-1.2, 1.6);
+    wheel(1.2, 1.6);
+
+    wheel(-1.2, -1.6);
+    wheel(1.2, -1.6);
+
+    return wheels;
+
+}
