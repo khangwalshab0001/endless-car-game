559dc3124212fdbd0ae0a07f586eef78a996cefd
Khangwal <anujkhangwal30@gmail.com>
2026-07-26 12:54:37 +0530

    Update player.js

--- models/player.js
+++ models/player.js
@@ -1,13 +1,14 @@
 import * as THREE from "https://cdn.jsdelivr.net/npm/three@0.160/build/three.module.js";
 import { createBody } from "./body.js";
-
+import { createWheels } from "./wheels.js";
 export function createPlayer() {
 
     const car = new THREE.Group();
 
     const body = createBody();
     car.add(body);
-
+    const wheels = createWheels();
+car.add(wheels);
     car.position.set(0, 0, 0);
 
     return car;
