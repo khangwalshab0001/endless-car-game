050bb317929748b8b5aa77923ff1d67e49cb4370
Khangwal <anujkhangwal30@gmail.com>
2026-07-26 15:02:45 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 import { scene, camera, renderer } from "./core/scene.js";
 import { createPlayer } from "./models/player.js";
 import { createRoad } from "./js/world/road.js";
+import { createMap } from "./world/map.js";
 const player = createPlayer();
 
 scene.add(player);
 createRoad(scene);
+createMap(scene);
 camera.position.set(0, 4, 10);
 camera.lookAt(player.position);
 function animate() {
