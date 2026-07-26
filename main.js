ff42b5689bbd2f5897e282a7aaaae71d6a6ada11
Khangwal <anujkhangwal30@gmail.com>
2026-07-26 10:02:44 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,32 @@
-export {};
+import {
+    scene,
+    camera,
+    renderer
+} from "./core/scene.js";
+
+camera.position.set(0, 5, 10);
+
+function animate() {
+
+    requestAnimationFrame(animate);
+
+    renderer.render(scene, camera);
+
+}
+
+animate();
+
+window.addEventListener("resize", () => {
+
+    renderer.setSize(
+        window.innerWidth,
+        window.innerHeight
+    );
+
+    camera.aspect =
+        window.innerWidth /
+        window.innerHeight;
+
+    camera.updateProjectionMatrix();
+
+});
