e8cbd01ecac25d423b773a83588ee040238e8942
Khangwal <anujkhangwal30@gmail.com>
2026-07-27 10:28:55 +0530

    Update main.js

--- main.js
+++ main.js
@@ -3,11 +3,25 @@ import { createPlayer } from "./models/player.js";
 import { createRoad } from "./js/world/road.js";
 import { createMap } from "./js/world/map.js";
 import { createHouse } from "./js/village/house.js";
+import { createTree } from "./js/village/tree.js";
 const player = createPlayer();
 
 scene.add(player);
 const house = createHouse();
 scene.add(house);
+// Village Trees
+for (let i = 0; i < 25; i++) {
+
+    const tree = createTree();
+
+    tree.position.set(
+        (Math.random() - 0.5) * 120,
+        0,
+        (Math.random() - 0.5) * 120
+    );
+
+    scene.add(tree);
+}
 createMap(scene);
 createRoad(scene);
 
