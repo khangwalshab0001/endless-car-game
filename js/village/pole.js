342bfa697de9e9a55a4bfbebe1ba78efb540d4d1
Khangwal <anujkhangwal30@gmail.com>
2026-07-27 10:57:40 +0530

    Create pole.js

---
+++ js/village/pole.js
@@ -0,0 +1,30 @@
+import * as THREE from "https://cdn.jsdelivr.net/npm/three@0.160/build/three.module.js";
+
+export function createPole() {
+
+    const pole = new THREE.Group();
+
+    // Pole
+    const body = new THREE.Mesh(
+        new THREE.CylinderGeometry(0.12, 0.15, 8, 8),
+        new THREE.MeshStandardMaterial({
+            color: 0x777777
+        })
+    );
+
+    body.position.y = 4;
+    pole.add(body);
+
+    // Top Bar
+    const bar = new THREE.Mesh(
+        new THREE.BoxGeometry(2.2, 0.15, 0.15),
+        new THREE.MeshStandardMaterial({
+            color: 0x555555
+        })
+    );
+
+    bar.position.y = 7.5;
+    pole.add(bar);
+
+    return pole;
+}
