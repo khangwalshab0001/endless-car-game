7c0e5a3eb8bb4c95970233f5e3bd44b917d3a159
Khangwal <anujkhangwal30@gmail.com>
2026-07-31 09:09:00 +0530

    Update tree.js

--- js/village/tree.js
+++ js/village/tree.js
@@ -15,16 +15,47 @@ export function createTree() {
     trunk.position.y = 1.5;
     tree.add(trunk);
 
-    // Leaves
-    const leaves = new THREE.Mesh(
-        new THREE.SphereGeometry(1.8, 16, 16),
-        new THREE.MeshStandardMaterial({
-            color: 0x228B22
-        })
-    );
+   // Main Leaves
+const leaves1 = new THREE.Mesh(
+    new THREE.SphereGeometry(1.8, 16, 16),
+    new THREE.MeshStandardMaterial({
+        color: 0x228B22
+    })
+);
 
-    leaves.position.y = 4;
-    tree.add(leaves);
+leaves1.position.set(0, 4.2, 0);
+tree.add(leaves1);
+
+// Left Leaves
+const leaves2 = leaves1.clone();
+leaves2.position.set(-1.2, 4.0, 0);
+tree.add(leaves2);
+
+// Right Leaves
+const leaves3 = leaves1.clone();
+leaves3.position.set(1.2, 4.0, 0);
+tree.add(leaves3);
+
+// Front Leaves
+const leaves4 = leaves1.clone();
+leaves4.position.set(0, 4.0, 1.2);
+tree.add(leaves4);
+
+// Back Leaves
+const leaves5 = leaves1.clone();
+leaves5.position.set(0, 4.0, -1.2);
+tree.add(leaves5);
+
+// Top Leaves
+const leaves6 = new THREE.Mesh(
+    new THREE.SphereGeometry(1.4, 16, 16),
+    new THREE.MeshStandardMaterial({
+        color: 0x2e8b57
+    })
+);
+
+leaves6.position.set(0, 5.5, 0);
+tree.add(leaves6);
 const collisionBox = new THREE.Mesh(
     new THREE.BoxGeometry(1.0, 3, 1.0),
     new THREE.MeshBasicMaterial({
