e20f62251feb7bfbbb8f3cba7f353a5b2301e73e
Khangwal <anujkhangwal30@gmail.com>
2026-08-02 23:40:51 +0530

    Update petrolPump.js

--- js/highway/petrolPump.js
+++ js/highway/petrolPump.js
@@ -25,6 +25,25 @@ export function createPetrolPump() {
 
     roof.position.set(0, 5.3, 0);
     pump.add(roof);
+// Fuel Dispenser 1
+const dispenser1 = new THREE.Mesh(
+    new THREE.BoxGeometry(0.8, 2, 0.8),
+    new THREE.MeshStandardMaterial({
+        color: 0xffffff
+    })
+);
 
+dispenser1.position.set(-2.5, 1, 5);
+pump.add(dispenser1);
+
+// Fuel Dispenser 2
+const dispenser2 = dispenser1.clone();
+dispenser2.position.x = 0;
+pump.add(dispenser2);
+
+// Fuel Dispenser 3
+const dispenser3 = dispenser1.clone();
+dispenser3.position.x = 2.5;
+pump.add(dispenser3);
     return pump;
 }
