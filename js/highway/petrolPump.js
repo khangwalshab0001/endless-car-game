77be47dec9032ffb3ee2e73f354d10a8e5d7b38d
Khangwal <anujkhangwal30@gmail.com>
2026-08-09 10:02:59 +0530

    Update petrolPump.js

--- js/highway/petrolPump.js
+++ js/highway/petrolPump.js
@@ -45,5 +45,41 @@ pump.add(dispenser2);
 const dispenser3 = dispenser1.clone();
 dispenser3.position.x = 2.5;
 pump.add(dispenser3);
+    // Petrol Pump Canopy
+const canopy = new THREE.Mesh(
+    new THREE.BoxGeometry(16, 0.5, 11),
+    new THREE.MeshStandardMaterial({
+        color: 0xd62828
+    })
+);
+
+canopy.position.set(0, 4, 5);
+pump.add(canopy);
+
+// Canopy Pillar 1
+const pillar1 = new THREE.Mesh(
+    new THREE.BoxGeometry(0.5, 4, 0.5),
+    new THREE.MeshStandardMaterial({
+        color: 0xffffff
+    })
+);
+
+pillar1.position.set(-6, 2, 1);
+pump.add(pillar1);
+
+// Canopy Pillar 2
+const pillar2 = pillar1.clone();
+pillar2.position.set(6, 2, 1);
+pump.add(pillar2);
+
+// Canopy Pillar 3
+const pillar3 = pillar1.clone();
+pillar3.position.set(-6, 2, 9);
+pump.add(pillar3);
+
+// Canopy Pillar 4
+const pillar4 = pillar1.clone();
+pillar4.position.set(6, 2, 9);
+pump.add(pillar4);
     return pump;
 }
